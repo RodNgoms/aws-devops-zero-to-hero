d8aaa3c72dc9dddd60603d4ae8e8bdfcca688a5e
RodNgoms <122399894+RodNgoms@users.noreply.github.com>
2024-06-04 14:32:14 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull rodngoms/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rodngoms/simple-python-flask-app:latest
+docker run -d -p 5000:5000 rodngoms/simple-python-flask-app:latest 
